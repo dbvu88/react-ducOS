78293a7ce0899a7f81ccadec0ee05653e4b46431
Duc Vu <de.yd315@gmail.com>
2019-04-21 08:53:53 -0500

    added second hand movement

--- src/components/home/Clock.js
+++ src/components/home/Clock.js
@@ -3,13 +3,50 @@ import React, { Component } from 'react';
 import './Clock.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="clock-display">
-        <div className='hand hand-hour'></div>        
-        <div className='hand hand-min'></div>        
-        <div className='hand hand-sec'></div>        
-      </div>
+    constructor() {
+        super()
+        const secAngle = this.getHandAngle(new Date().getSeconds(), 60)
+        this.state = {
+            secAngle: secAngle,
+        }
+    }
+
+    componentWillMount = () => {
+        setInterval(this.getCurrentTime, 1000)
+    }
+    getCurrentTime = () => {
+        console.log(this.state.secAngle)
+        const secAngle = this.getHandAngle(new Date().getSeconds(), 60)
+        this.setState({
+            secAngle: secAngle,
+        })
+    }
+
+    getHandAngle = (value, tick) => (
+        {
+            transform: `rotate(${parseInt(value)*360/tick + 90}deg)`
+        }
+    )
+
+    
+
+
+    render() {
+        return (
+        <div className="clock-display">
+            <div 
+            className='hand hand-hour'
+            style={this.state.hourAngle}
+            ></div>        
+            <div 
+            className='hand hand-min'
+            style={this.state.minAngle}
+            ></div>        
+            <div 
+            className='hand hand-sec'
+            style={this.state.secAngle}
+            ></div>        
+        </div>
     );
   }
 }
