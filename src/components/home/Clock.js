5e7cea7d53202fdaf4d3f51e93bb80a6586cf3fe
Duc Vu <de.yd315@gmail.com>
2019-04-21 07:36:37 -0500

    added clock styling

--- src/components/home/Clock.js
+++ src/components/home/Clock.js
@@ -6,8 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="clock-display">
-        Clock
-
+        <div className='hand hand-hour'></div>        
+        <div className='hand hand-min'></div>        
+        <div className='hand hand-sec'></div>        
       </div>
     );
   }
