fe5f36b9d8d9e8e3a33a50f23c39c6d8b8b3b374
Duc Vu <de.yd315@gmail.com>
2019-04-21 09:08:18 -0500

    added hour hand movement

--- src/components/home/Clock.js
+++ src/components/home/Clock.js
@@ -6,8 +6,11 @@ class App extends Component {
     constructor() {
         super()
         const secAngle = this.getHandAngle(new Date().getSeconds(), 60)
+        const hourAngle = this.getHandAngle(new Date().getSeconds(), 60)
+        // const minAngle = this.getHandAngle(new Date().getMinutes(), )
         this.state = {
             secAngle: secAngle,
+            hourAngle: hourAngle
         }
     }
 
@@ -15,10 +18,13 @@ class App extends Component {
         setInterval(this.getCurrentTime, 1000)
     }
     getCurrentTime = () => {
-        console.log(this.state.secAngle)
+        // console.log(this.state.secAngle)
         const secAngle = this.getHandAngle(new Date().getSeconds(), 60)
+        const hourAngle = this.getHandAngle(new Date().getSeconds(), 60)
+
         this.setState({
             secAngle: secAngle,
+            hourAngle: hourAngle
         })
     }
 
