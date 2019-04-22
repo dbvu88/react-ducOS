2c8fd90d4c6a1693bc934dc54e3d24eebc4b7747
Duc Vu <de.yd315@gmail.com>
2019-04-22 10:28:18 -0500

    fixed about app content

--- src/apps/about/app.js
+++ src/apps/about/app.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App-container">
         <article className='application-about-article'>
           <h2>Welcome to DucOS.</h2>
-          <p>DucOS is a Web Portfolio that I developed to looks and feels like an actual Operating System.</p>
+          <p>DucOS is a Web Portfolio that I developed to look and feel like an actual Operating System.</p>
           <p>However, I do hope that it can become an actual Web Operating System eventually.</p>
           <p>Therefore, I will continue to integrate more  awesome features and amazing applications to DucOS.</p>
           <p>Please stay tuned for upcoming features and DucOS applications!!</p>
