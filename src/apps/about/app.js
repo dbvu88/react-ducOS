c764d0e628747fa479a8035ada1db1907d88e017
Duc Vu <de.yd315@gmail.com>
2019-04-21 23:43:36 -0500

    started working on about app

--- src/apps/about/app.js
+++ src/apps/about/app.js
@@ -1,12 +1,16 @@
 
 import React, { Component } from 'react';
+import './styles.css'
 
 class App extends Component {
   render() {
     console.log('about')
     return (
       <div className="App-screen">
-        About
+        <article className='application-about-article'>
+          <h2>DucOS</h2>
+          <p>DucOS is an OS-themed web portfolios developed by Duc Vu. The </p>
+        </article>
       </div>
     );
   }
