c838999adb8347ea2844d76ea6ec31fa0e25b187
Duc Vu <de.yd315@gmail.com>
2019-04-24 04:03:56 -0500

    updated about page

--- src/apps/about/app.js
+++ src/apps/about/app.js
@@ -2,6 +2,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './styles.css'
+import linkedin from './social-media-icon/linkedin.png'
+import github from './social-media-icon/github.png'
+import gmail from './social-media-icon/gmail.png'
+import twitter from './social-media-icon/twitter.png'
 
 class App extends Component {
   render() {
@@ -14,15 +18,21 @@ class App extends Component {
           <p>However, I do hope that it can become an actual Web Operating System eventually.</p>
           <p>Therefore, I will continue to integrate more  awesome features and amazing applications to DucOS.</p>
           <p>Please stay tuned for upcoming features and DucOS applications!!</p>
-          <p>Please stay tuned for upcoming features and DucOS applications!!</p>
           <p><Link to='/apps/upcoming-changes'>Click here to read my notes on upcoming changes!</Link></p>
-          <p>You can contact me at:</p>
-          <ul>
+          <p>I also love to connect with others, please contact me at:</p>
+          
+          <a href='https://www.linkedin.com/in/dbv135/'><img className='social-media' src={linkedin} /></a>
+          <a href='https://github.com/vud531'><img className='social-media' src={github} /></a>
+          <a href='https://twitter.com/DucVu99'><img className='social-media' src={twitter} /></a>
+          <a href='mailto: ducbavu531@gmail.com'><img className='social-media' src={gmail} />ducbavu531@gmail.com</a>
+          {/* <a href='https://codepen.io/vud531/'><img className='social-media' src={codepen} /></a> */}
+
+          {/* <ul>
             <li><a href='https://www.linkedin.com/in/dbv135/'>LinkedIn</a></li>
             <li><a href='https://github.com/vud531'>GitHub</a></li>
             <li><a href='https://twitter.com/DucVu99'>Twitter</a></li>
             <li>Email: ducbavu531@gmail.com</li>
-          </ul>
+          </ul> */}
 
 
         </article>
